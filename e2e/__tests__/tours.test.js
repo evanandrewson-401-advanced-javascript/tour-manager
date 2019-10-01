71ea435f35a7ad4b5b7c74e944e1381ee40a806b
evanandrewson <evanandrewson@gmail.com>
2019-10-01 14:43:56 -0700

    added get by id route for tours and corresponding test

--- e2e/__tests__/tours.test.js
+++ e2e/__tests__/tours.test.js
@@ -10,7 +10,7 @@ describe('tours api', () => {
     title: 'North American Tour',
     activities: ['music', 'food', 'beer'],
     stops: []
-  }
+  };
 
   function postTour(tour) {
     return request
@@ -31,4 +31,31 @@ describe('tours api', () => {
     });
   });
 
+  it('gets a tour by id', () => {
+    return postTour(northAmericanTour)
+      .then(tour => {
+        return request.get(`/api/tours/${tour._id}`).expect(200);
+      })
+      .then(({ body }) => {
+        expect(body).toMatchInlineSnapshot(
+          {
+            _id: expect.any(String)
+          },
+          `
+          Object {
+            "__v": 0,
+            "_id": Any<String>,
+            "activities": Array [
+              "music",
+              "food",
+              "beer",
+            ],
+            "launchDate": "2019-10-01T21:43:21.429Z",
+            "stops": Array [],
+            "title": "North American Tour",
+          }
+        `
+        );
+      });
+  });
 });

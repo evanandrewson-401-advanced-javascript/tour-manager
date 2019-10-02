9a405af47f96efbd0c2ff5792e5adf369a9ac548
evanandrewson <evanandrewson@gmail.com>
2019-10-02 07:03:24 -0700

    added put route for stop and corresponding test

--- e2e/__tests__/stops.test.js
+++ e2e/__tests__/stops.test.js
@@ -40,6 +40,7 @@ describe('stops api', () => {
         });
     });
   });
+
   it('deletes a stop', () => {
     return postTour(northAmericanTour).then(tour => {
       return request
@@ -49,7 +50,42 @@ describe('stops api', () => {
         .then(stop => {
           return request
             .delete(`/api/tours/${tour._id}/stops/${stop._id}`)
+            .expect(200);
+        });
+    });
+  });
+
+  it('updates a stop with attendees', () => {
+    return postTour(northAmericanTour).then(tour => {
+      return request
+        .post(`/api/tours/${tour._id}/stops`)
+        .expect(200)
+        .send({ address: 97214 })
+        .then(({ body }) => {
+          return request
+            .put(`/api/tours/${tour._id}/stops/${body._id}`)
             .expect(200)
+            .send({ attendance: 50 })
+            .then(stop => {
+              console.log(stop.body);
+              expect(stop.body).toMatchInlineSnapshot(
+                {
+                  _id: expect.any(String),
+                  weather: expect.any(Array)
+                },
+                `
+                Object {
+                  "__v": 0,
+                  "_id": Any<String>,
+                  "location": Object {
+                    "latitude": "45.5113506",
+                    "longitude": "-122.6456739",
+                  },
+                  "weather": Any<Array>,
+                }
+              `
+              );
+            });
         });
     });
   });
